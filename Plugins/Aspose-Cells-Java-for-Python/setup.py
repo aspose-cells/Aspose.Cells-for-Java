1bff9569411f638200e058746f921357907f6ee4
Fahad Adeel <fahadadeel@gmail.com>
2016-03-03 18:29:58 +0500

    Aspose.Cells Java for Python - Updated

---
+++ Plugins/Aspose-Cells-Java-for-Python/setup.py
@@ -0,0 +1,19 @@
+__author__ = 'fahadadeel'
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'aspose-cells-java-for-python',
+    packages = find_packages(),
+    version = '1.0',
+    description = 'Aspose.cells Java for Python is a project that demonstrates / provides the Aspose.Cells for Java API usage examples in Python.',
+    author='Fahad Adeel',
+    author_email='cells@aspose.com',
+    url='https://github.com/asposecells/Aspose_Cells_Java/tree/master/Plugins/Aspose-Cells-Java-for-Python',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
