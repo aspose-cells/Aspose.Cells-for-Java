c0438d7b9b1ac543f2599fe120515445a3110e66
Fahad Adeel <fahadadeel@gmail.com>
2016-12-02 11:20:33 +0500

    Aspose.Cells for Java New Release 16.11.0 Examples

---
+++ Examples/src/main/java/com/aspose/cells/examples/articles/CreateStyleobjectusingCellsFactoryclass.java
@@ -0,0 +1,32 @@
+package com.aspose.cells.examples.articles;
+
+import com.aspose.cells.BackgroundType;
+import com.aspose.cells.CellsFactory;
+import com.aspose.cells.Color;
+import com.aspose.cells.Style;
+import com.aspose.cells.Workbook;
+import com.aspose.cells.examples.Utils;
+
+public class CreateStyleobjectusingCellsFactoryclass {
+	public static void main(String[] args) throws Exception {
+
+		// The path to the documents directory.
+		String dataDir = Utils.getSharedDataDir(CreateStyleobjectusingCellsFactoryclass.class) + "articles/";
+
+		// Create a Style object using CellsFactory class
+		CellsFactory cf = new CellsFactory();
+		Style st = cf.createStyle();
+
+		// Set the Style fill color to Yellow
+		st.setPattern(BackgroundType.SOLID);
+		st.setForegroundColor(Color.getYellow());
+
+		// Create a workbook and set its default style using the created Style
+		// object
+		Workbook wb = new Workbook();
+		wb.setDefaultStyle(st);
+
+		// Save the workbook
+		wb.save(dataDir + "CreateStyleobject_out.xlsx");
+	}
+}
---
+++ Examples/src/main/java/com/aspose/cells/examples/articles/CustomSettings.java
@@ -0,0 +1,52 @@
+package com.aspose.cells.examples.articles;
+
+import java.util.Locale;
+
+import com.aspose.cells.ConsolidationFunction;
+import com.aspose.cells.GlobalizationSettings;
+
+public class CustomSettings extends GlobalizationSettings {
+	public String getTotalName(int functionType) {
+		switch (functionType) {
+		case ConsolidationFunction.AVERAGE:
+			return "AVG";
+
+		// Handle other cases
+
+		default:
+			return super.getTotalName(functionType);
+		}
+	}
+
+	public String getGrandTotalName(int functionType) {
+		switch (functionType) {
+		case ConsolidationFunction.AVERAGE:
+			return "GRAND AVG";
+
+		// Handle other cases
+
+		default:
+			return super.getGrandTotalName(functionType);
+		}
+	}
+	
+	public String getOtherName()
+    {
+        String language = Locale.getDefault().getLanguage();
+		System.out.println(language);
+		switch (language)
+		{
+		    case "en":
+		        return "Other";
+		    case "fr":
+		        return "Autre";
+		    case "de":
+		        return "Andere";
+		
+		    //Handle other cases as per requirement
+		
+		    default:
+		        return super.getOtherName();
+		}
+    }
+}
