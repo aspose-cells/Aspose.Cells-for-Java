a11a78387c2692d84b0a7c63b8fd5a88fd3f4092
Shakeel Faiz <shakeel.faiz@aspose.com>
2017-06-22 09:41:17 +0500

    Aspose.Cells for Java 17.6 - Code Examples
    Following Example Added
    -----------------------
    TilePictureAsTextureInsideShape.java

---
+++ Examples/src/AsposeCellsExamples/DrawingObjects/TilePictureAsTextureInsideShape.java
@@ -0,0 +1,31 @@
+package AsposeCellsExamples.DrawingObjects;
+
+import com.aspose.cells.*;
+import AsposeCellsExamples.Utils;
+
+public class TilePictureAsTextureInsideShape {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Aspose.Cells for Java Version: " + CellsHelper.getVersion());
+		
+		String srcDir = Utils.Get_SourceDirectory();
+		String outDir = Utils.Get_OutputDirectory();
+	
+		//Load sample Excel file
+		Workbook wb = new Workbook(srcDir + "sampleTextureFill_IsTiling.xlsx");
+		  
+		//Access first worksheet
+		Worksheet ws = wb.getWorksheets().get(0);
+		  
+		//Access first shape inside the worksheet
+		Shape sh = ws.getShapes().get(0);
+		  
+		//Tile Picture as a Texture inside the Shape 
+		sh.getFill().getTextureFill().setTiling(true);
+		  
+		//Save the output Excel file
+		wb.save(outDir + "outputTextureFill_IsTiling.xlsx");
+
+		//Print the message
+		System.out.println("TilePictureAsTextureInsideShape executed successfully.");
+	}
+}
