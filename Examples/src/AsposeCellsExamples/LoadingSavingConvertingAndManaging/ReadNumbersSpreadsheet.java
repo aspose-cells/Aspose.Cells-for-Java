aac5fdc529666f3f2117d6c312a1dc81fd10be3e
Shakeel Faiz <shakeel.faiz@aspose.com>
2017-08-03 15:40:13 +0500

    Commit Intermediate - Upgrade to 17.7

---
+++ Examples/src/AsposeCellsExamples/LoadingSavingConvertingAndManaging/ReadNumbersSpreadsheet.java
@@ -0,0 +1,25 @@
+package AsposeCellsExamples.LoadingSavingConvertingAndManaging;
+
+import com.aspose.cells.*;
+import AsposeCellsExamples.Utils;
+
+public class ReadNumbersSpreadsheet {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Aspose.Cells for Java Version: " + CellsHelper.getVersion());
+		
+		String srcDir = Utils.Get_SourceDirectory();
+		String outDir = Utils.Get_OutputDirectory();
+
+		//Specify load options, we want to load Numbers spreadsheet.
+		LoadOptions opts = new LoadOptions(LoadFormat.NUMBERS);
+		  
+		//Load the Numbers spreadsheet in workbook with above load options.
+		Workbook wb = new Workbook(srcDir + "sampleNumbersByAppleInc.numbers", opts);
+		  
+		//Save the workbook to pdf format
+		wb.save(outDir + "outputNumbersByAppleInc.pdf", SaveFormat.PDF);
+
+		//Print the message
+		System.out.println("ReadNumbersSpreadsheet executed successfully.");
+	}
+}
