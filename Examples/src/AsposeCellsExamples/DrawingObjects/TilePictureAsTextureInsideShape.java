34bc8aa32ad5aaf30df2753540b9fd0a84272a0d
Shakeel Faiz <shakeel.faiz@aspose.com>
2017-12-11 17:55:31 +0500

    PasswordProtectOrUnprotectSharedWorkbook

--- Examples/src/AsposeCellsExamples/DrawingObjects/TilePictureAsTextureInsideShape.java
+++ Examples/src/AsposeCellsExamples/DrawingObjects/TilePictureAsTextureInsideShape.java
@@ -4,13 +4,13 @@ import com.aspose.cells.*;
 import AsposeCellsExamples.Utils;
 
 public class TilePictureAsTextureInsideShape {
+	
+	static String srcDir = Utils.Get_SourceDirectory();
+	static String outDir = Utils.Get_OutputDirectory();
 
 	public static void main(String[] args) throws Exception {
 
 		System.out.println("Aspose.Cells for Java Version: " + CellsHelper.getVersion());
-
-		String srcDir = Utils.Get_SourceDirectory();
-		String outDir = Utils.Get_OutputDirectory();
 
 		// Load sample Excel file
 		Workbook wb = new Workbook(srcDir + "sampleTextureFill_IsTiling.xlsx");
---
+++ Examples/src/AsposeCellsExamples/Workbook/PasswordProtectOrUnprotectSharedWorkbook.java
@@ -0,0 +1,30 @@
+package AsposeCellsExamples.Workbook;
+
+import com.aspose.cells.*;
+import AsposeCellsExamples.Utils;
+
+public class PasswordProtectOrUnprotectSharedWorkbook { 
+	
+	static String srcDir = Utils.Get_SourceDirectory();
+	static String outDir = Utils.Get_OutputDirectory();
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("Aspose.Cells for Java Version: " + CellsHelper.getVersion());
+			
+		//Create empty Excel file
+		Workbook wb = new Workbook();
+
+		//Protect the Shared Workbook with Password
+		wb.protectSharedWorkbook("1234");
+
+		//Uncomment this line to Unprotect the Shared Workbook
+		//wb.unprotectSharedWorkbook("1234");
+
+		//Save the output Excel file
+		wb.save(outDir + "outputProtectSharedWorkbook.xlsx");
+		
+		// Print the message
+		System.out.println("SetValuesFormatCodeOfChartSeries executed successfully.");
+	}
+}
