cf592a85ce352cb393b10f0bb64fe444180ae9e9
Shakeel Faiz <shakeel.faiz@aspose.com>
2018-01-05 14:20:25 +0500

    RunExamples

--- Examples/src/AsposeCellsExamples/RunExamples.java
+++ Examples/src/AsposeCellsExamples/RunExamples.java
@@ -6,12 +6,17 @@ public class RunExamples {
 
 		// Uncomment the one you want to try out
 		
+		//Aspose.Cells for Java v17.12
+		AsposeCellsExamples.Charts.SetShapeTypeOfDataLabelsOfChart.main(null);
+		AsposeCellsExamples.HTML.ExportWorksheetCSSSeparatelyInOutputHTML.main(null);
+		AsposeCellsExamples.HTML.PrefixTableElementsStylesWithHtmlSaveOptions_TableCssIdProperty.main(null);
+		
 		//Aspose.Cells for Java v17.9
-		AsposeCellsExamples.Rendering.AddPDFBookmarksWithNamedDestinations.main(null);
-		AsposeCellsExamples.Rendering.ControlLoadingOfExternalResourcesInExcelToPDF.main(null);
-		AsposeCellsExamples.WorkbookVBAProject.CopyVBAMacroUserFormDesignerStorageToWorkbook.main(null);
-		AsposeCellsExamples.DrawingObjects.SendShapeFrontOrBackInWorksheet.main(null);
-		AsposeCellsExamples.Data.SortDataInColumnWithCustomSortList.main(args);;
+		//AsposeCellsExamples.Rendering.AddPDFBookmarksWithNamedDestinations.main(null);
+		//AsposeCellsExamples.Rendering.ControlLoadingOfExternalResourcesInExcelToPDF.main(null);
+		//AsposeCellsExamples.WorkbookVBAProject.CopyVBAMacroUserFormDesignerStorageToWorkbook.main(null);
+		//AsposeCellsExamples.DrawingObjects.SendShapeFrontOrBackInWorksheet.main(null);
+		//AsposeCellsExamples.Data.SortDataInColumnWithCustomSortList.main(args);
 
 	}
 
