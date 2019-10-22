86b312af1ff5c015a9c749331772c36a2bcd004b
Ahmad <m.ahmadchishti@gmail.com>
2019-10-22 16:13:11 +0500

    Update SetBackgroundPictureforWorksheet.java

--- Examples/src/AsposeCellsExamples/TechnicalArticles/SetBackgroundPictureforWorksheet.java
+++ Examples/src/AsposeCellsExamples/TechnicalArticles/SetBackgroundPictureforWorksheet.java
@@ -9,16 +9,17 @@ import java.io.*;
 
 public class SetBackgroundPictureforWorksheet {
 	public static void main(String[] args) throws Exception {
-
-		// The path to the documents directory.
-		String dataDir = Utils.getSharedDataDir(SetBackgroundPictureforWorksheet.class) + "TechnicalArticles/";
+		// ExStart:1
+		// The path to the directories.
+		String sourceDir = Utils.Get_SourceDirectory();
+		String outDir = Utils.Get_OutputDirectory();
 		// Instantiate a new Workbook.
 		Workbook workbook = new Workbook();
 		// Get the first worksheet.
 		Worksheet sheet = workbook.getWorksheets().get(0);
 
 		// Get the image file.
-		File file = new File(dataDir + "school.jpg");
+		File file = new File(sourceDir + "background.png");
 		// Get the picture into the streams.
 		byte[] imageData = new byte[(int) file.length()];
 		FileInputStream fis = new FileInputStream(file);
@@ -29,7 +30,7 @@ public class SetBackgroundPictureforWorksheet {
 
 		fis.close();
 		// Save the excel file
-		workbook.save(dataDir + "SBPforWorksheet.xls");
-
+		workbook.save(outDir + "SBPforWorksheet.xlsx");
+		// ExEnd:1
 	}
 }
