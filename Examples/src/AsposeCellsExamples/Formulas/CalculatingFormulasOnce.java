08d1f6373f1088c52076dc593cb4d65517dfbf16
johnson.shi <johnson.shi@aspose.com>
2021-12-01 17:10:05 +0800

    improved code for using calculation chain

--- Examples/src/AsposeCellsExamples/Formulas/CalculatingFormulasOnce.java
+++ Examples/src/AsposeCellsExamples/Formulas/CalculatingFormulasOnce.java
@@ -1,7 +1,9 @@
 package AsposeCellsExamples.Formulas;
 
+import com.aspose.cells.Cells;
 import com.aspose.cells.DateTime;
 import com.aspose.cells.Workbook;
+
 import AsposeCellsExamples.Utils;
 
 public class CalculatingFormulasOnce {
@@ -14,12 +16,26 @@ public class CalculatingFormulasOnce {
 		// Print the time before formula calculation
 		System.out.println(DateTime.getNow());
 
-		// Set the CreateCalcChain as false
-		workbook.getSettings().setCreateCalcChain(false);
+		// Set the CreateCalcChain as true
+		workbook.getSettings().setCreateCalcChain(true);
 
 		// Calculate the workbook formulas
 		workbook.calculateFormula();
 
+		Cells cells = workbook.getWorksheets().get("Sheet1").getCells();
+		
+		//with original values, the calculated result
+        System.out.println(cells.get("A11").getValue());
+        
+        //update one value the formula depends on
+		cells.get("A5").putValue(15);
+		
+        // Calculate the workbook formulas again, in fact only A11 needs to be and will be calculated
+        workbook.calculateFormula();
+
+        //check the re-calculated value
+        System.out.println(cells.get("A11").getValue());
+        
 		// Print the time after formula calculation
 		System.out.println(DateTime.getNow());
 	}
