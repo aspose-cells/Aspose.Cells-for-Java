644d106baecdf2365bd8470d16a8bb5c8188bb46
peter <peter.zhou@aspose.com>
2025-08-13 15:10:02 +0800

    update gridjs java demo to v25.8

--- Examples.GridJs/src/main/java/com/aspose/gridjs/demo/MyConfig.java
+++ Examples.GridJs/src/main/java/com/aspose/gridjs/demo/MyConfig.java
@@ -2,13 +2,9 @@ package com.aspose.gridjs.demo;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.aspose.gridjs.GridJsOptions;
-import com.aspose.gridjs.GridJsService;
-import com.aspose.gridjs.GridJsWorkbook;
-import com.aspose.gridjs.IGridJsService;
 
 @Configuration
 public class MyConfig {
@@ -21,20 +17,12 @@ public class MyConfig {
 	
 	
 	@Bean
-    public GridJsWorkbook gridJsWorkbook() {
-        return new GridJsWorkbook();  
-    }
-	
-	
-    @Bean
     public GridJsOptions gridJsOptions() {
     	GridJsOptions options=new GridJsOptions();
     	options.setFileCacheDirectory(cachePath);
         return options;
     }
 
-    @Bean
-    public IGridJsService gridJsService(GridJsOptions gridJsOptions) throws Exception {
-        return new GridJsService(gridJsOptions);
-    }
+
+ 
 }
